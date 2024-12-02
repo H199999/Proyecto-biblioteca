f736e422d2e03b422dbc291cd71351460001dc54
H199999 <catalinaa.h992@gmail.com>
2024-12-02 05:36:55 -0300

    2

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'biblioteca.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'H19$default',
+        'USER': 'H19',
+        'PASSWORD': 'prestamos1234',
+        'HOST': 'H19.mysql.pythonanywhere-services.com',
+        'PORT': '3306'
     }
 }
 
